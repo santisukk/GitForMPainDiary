895aaa64d5fd73f1dad9dcdf71ab55f2bde52cc9
santi <https://github.com/santisukk/testGitForAndroid.git>
2017-01-11 11:34:51 +0700

    A

--- app/src/main/java/kku/mhealth/hackathon/mpaindiary/DentLoginActivity.java
+++ app/src/main/java/kku/mhealth/hackathon/mpaindiary/DentLoginActivity.java
@@ -17,74 +17,84 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class DentLoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText emailEditText,passEditText;
-    private Button loginButton;
+//    private EditText emailEditText,passEditText;
+//    private Button loginButton;
+//
+//    private ProgressDialog progressDialog;
+//    private FirebaseAuth firebaseAuth;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_dent_login);
+//
+//        firebaseAuth = FirebaseAuth.getInstance();
+//
+//        if(firebaseAuth.getCurrentUser() != null){
+//            //After login activity
+//            finish();
+//            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//        }
+//
+//        progressDialog = new ProgressDialog(this);
+////        loginButton = (Button)findViewById(R.id.button2);
+////        emailEditText = (EditText)findViewById(R.id.editText);
+////        passEditText = (EditText)findViewById(R.id.editText2);
+//
+//        loginButton.setOnClickListener(this);
+//
+//    }
+//
+//
+//    @Override
+//    public void onClick(View v) {
+//        if(v == loginButton){
+//            loginDentUser();
+//        }
+//    }
+//
+//    private void loginDentUser() {
+//        String email = emailEditText.getText().toString().trim();
+//        String password = passEditText.getText().toString().trim();
+//
+//        if(email.isEmpty()){
+//            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        if(password.isEmpty()){
+//            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        if(password.length() < 6){
+//            Toast.makeText(this, "Passwords must be at least 6 characters", Toast.LENGTH_SHORT).show();
+//            return;
+//        }
+//        progressDialog.setMessage("Login User..." + email + " : " + password);
+//        progressDialog.show();
+//
+//        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                progressDialog.dismiss();
+//                if(task.isSuccessful()){
+//                    Toast.makeText(DentLoginActivity.this, "Login Complete", Toast.LENGTH_SHORT).show();
+//                    finish();
+//                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//                }
+//                else{
+//                    Toast.makeText(DentLoginActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
 
-    private ProgressDialog progressDialog;
-    private FirebaseAuth firebaseAuth;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dent_login);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-
-        if(firebaseAuth.getCurrentUser() != null){
-            //After login activity
-            finish();
-            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-        }
-
-        progressDialog = new ProgressDialog(this);
-        loginButton = (Button)findViewById(R.id.button2);
-        emailEditText = (EditText)findViewById(R.id.editText);
-        passEditText = (EditText)findViewById(R.id.editText2);
-
-        loginButton.setOnClickListener(this);
-
     }
-
 
     @Override
     public void onClick(View v) {
-        if(v == loginButton){
-            loginDentUser();
-        }
-    }
 
-    private void loginDentUser() {
-        String email = emailEditText.getText().toString().trim();
-        String password = passEditText.getText().toString().trim();
-
-        if(email.isEmpty()){
-            Toast.makeText(this, "Please enter email", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(password.isEmpty()){
-            Toast.makeText(this, "Please enter password", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        if(password.length() < 6){
-            Toast.makeText(this, "Passwords must be at least 6 characters", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        progressDialog.setMessage("Login User..." + email + " : " + password);
-        progressDialog.show();
-
-        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                progressDialog.dismiss();
-                if(task.isSuccessful()){
-                    Toast.makeText(DentLoginActivity.this, "Login Complete", Toast.LENGTH_SHORT).show();
-                    finish();
-                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                }
-                else{
-                    Toast.makeText(DentLoginActivity.this, "Try Again", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
 }
